3d943db8fdd9af644646f6491bc30d6d6498e2f4
HeroesOfBalkan <markomacura003@gmail.com>
2022-01-16 14:39:26 +0100

    jos toga

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from venv import main
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +8,5 @@ def index():
     return "cao"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    PORT = os.environ.get("PORT", 5000)
+    app.run(debug=True, host="0.0.0.0", port = PORT)
